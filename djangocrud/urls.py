3bde190d05651a288c1428ed1ea160436e4826ab
favian silva <fabiansil902@gmail.com>
2025-06-18 16:48:10 -0500

    estuve viendo la creacion de formularios por django ademas de como este recibia la informacion para pasarla a la base de datos user

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,6 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloword),
-    path('signup/', views.helloword)
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 
 # Create your views here.
 
-def helloword(request):
-    return render(request, 'signup.html', {
-        'form' : UserCreationForm()
+def home(request):
+    return render(request, 'home.html', {
     })
+    
+def signup(request):
+    if request.method == 'GET':
+        return render (request, 'signup.html', {'form': UserCreationForm }) #ese "UserCreationForm" es para crear un formulario que ya viene por defecto de django y utilizarlo en nuestra app
+    else:
+        if request.POST['password1'] == request.POST['password2']:#aqui estamos comparando las dos comtraseña que nos esta dando el usuario en el formulario de django
+            try:#aqui se atrapa el error
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])#creamos una variable donde va atener la informacion del usuario que se esta creando, es decir que del espacio que dice "username" es el espacio que existe ya en la base de datos y ademas se le coloca otra vez "username" que esta se encuantra en la plantilla del html, al momento de que el espacio del 'username' le llegue un dato este dato lo enviara a la base de datos que esta como 'username'
+                #User.objects.create_user: es para crear una tabla en la base de datos que sea para los usuarios que se estan creando en el formulario de django
+                user.save()#aqui estamos diciendo que los datos creados se deben guardar
+                return HttpResponse('usuario creado')
+            except:#muestra el error
+                return HttpResponse('username ya existe')
+    return HttpResponse('password no coinciden')
