8184e4206ccae02a9f660329d1ba11ae3409769a
favian silva <fabiansil902@gmail.com>
2025-06-20 15:33:50 -0500

    estuve viendo como hacer el login para que ingresaran las personas que ya estan autenticadas por la aplicacion y puedan ver ciertas vistas creadas solo para esas personas autenticadas las que no estan deben de ver otras vistas

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+#el "UserCreationForm" es para crear un usuario y el "AuthenticationForm" es para cpprobar si el usuario existe
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError #esta biblioteca funciona para mostrar los errores mas detallados que hayamos tenido en la base de datos es decir si estaba haciendo una consulta o enviado datos a este
 from django.http import HttpResponse
 
@@ -21,7 +22,7 @@ def signup(request):
                 user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])#creamos una variable donde va atener la informacion del usuario que se esta creando, es decir que del espacio que dice "username" es el espacio que existe ya en la base de datos y ademas se le coloca otra vez "username" que esta se encuantra en la plantilla del html, al momento de que el espacio del 'username' le llegue un dato este dato lo enviara a la base de datos que esta como 'username'
                 #User.objects.create_user: es para crear una tabla en la base de datos que sea para los usuarios que se estan creando en el formulario de django
                 user.save()#aqui estamos diciendo que los datos creados se deben guardar
-                login(request, user)#este login crea un topo de autenticacion para el usuario que se acabo de crear para que la aplicacion sepa que usuario se tiene en la base de datos y sepamos que hace en nuestra aplicacion
+                login(request, user)#este login crea un tipo de autenticacion para el usuario que se acabo de crear para que la aplicacion sepa que usuario se tiene en la base de datos y sepamos que hace en nuestra aplicacion
                 return redirect('tasks')
             except IntegrityError:#muestra el error que haya ocurrido internamente en la base de datos
                 return render (request, 'signup.html', {
@@ -39,3 +40,20 @@ def tasks(request):
 def signout(request):
     logout(request)
     return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+        'form': AuthenticationForm
+    })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:#aqui mira mos si el usurio que esta intentando ingresar esta de alguna manera bacio es decir que no tiene la autenticacion o no esta en la base de datos lanza un error
+            return render(request, 'signin.html', {
+        'form': AuthenticationForm,
+        'error' : 'nombre o contraseña incorrecto '
+    })
+        else:
+            login(request, user)#este login crea un tipo de autenticacion para el usuario que se acabo de crear para que la aplicacion sepa que usuario se tiene en la base de datos y sepamos que hace en nuestra aplicacion
+            return redirect('tasks')
